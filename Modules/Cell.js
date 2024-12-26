443901e1f7abe7ea5b5f2cb290d5e9d7676e4f42
BoaN235 <noahgoata@outlook.com>
2024-12-26 11:18:54 -0500

    Add SVG assets for chess pieces and update Cell class for color management

--- Modules/Cell.js
+++ Modules/Cell.js
@@ -3,19 +3,26 @@ export class Cell {
         this.col = col;
         this.row = row;
         this.cell = document.createElement('div');
+        this.color = "#769656";
     }
     draw() {
         this.cell.classList.add('grid-square');
         if ((this.col + this.row) % 2 === 0) {
             this.cell.style.setProperty('--cell-color', '#769656');
+            this.color = "#769656";
         } else {
             this.cell.style.setProperty('--cell-color', '#eeeed2');
+            this.color = "#eeeed2";
         }
         document.querySelector('.grid-container').appendChild(this.cell);
         this.cell.addEventListener('click', this.onClick.bind(this));
     }
     onClick() {
         console.log(`Cell ${this.col}, ${this.row} clicked`);
-        this.cell.style.setProperty('--cell-color', '#ff6060');
+        if (this.cell.style.getPropertyValue('--cell-color') === '#ff6060') {
+            this.cell.style.setProperty('--cell-color', this.color);
+        } else {
+            this.cell.style.setProperty('--cell-color', '#ff6060');
+        }
     }
 }
